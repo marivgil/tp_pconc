a4aa53d5184f34e7b2611aefea6c0c08989bef4f
facundovigo <facundovigo@gmail.com>
2017-10-31 19:27:55 -0300

    Esto mas o menos se me ocurre seguro no es asi jaja pero se me ocurrrio esto

--- src/main/java/Barrier.java
+++ src/main/java/Barrier.java
@@ -1,4 +1,22 @@
 
 
 public class Barrier {
+
+    private int cantidadT;
+    private List<PerfectWorker> hilosABloquear = new ArrayList<PerfectWorker>();
+
+    public Barrier(int cantidadThreads){ cantidadT=cantidadThreads; }
+
+    public synchronized void barrera(PerfectWorker perfetWorker){
+
+        while(this.hilosABloquear.length != this.cantidadT )
+            try {
+                perfetWorker.wait();
+                hilosABloquear.add(perfetWorker);
+            } catch (InterruptedException e) {
+                e.getMessage();
+            }
+        notifyAll();
+
+    }
 }
